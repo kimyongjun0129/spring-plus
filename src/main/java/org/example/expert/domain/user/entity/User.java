e8c68be71625db8ebe1b687ccbee0ceffaec35f5
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-25 17:47:57 +0900

    레벨 2: JWT의 이해

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -20,9 +20,11 @@ public class User extends Timestamped {
     private String password;
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
+    private String nickname;
 
-    public User(String email, String password, UserRole userRole) {
+    public User(String email,  String nickname, String password, UserRole userRole) {
         this.email = email;
+        this.nickname = nickname;
         this.password = password;
         this.userRole = userRole;
     }
